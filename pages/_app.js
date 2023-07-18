356f43df0e1bf8a34e8a4dbe0bb7873f965f3153
samialam8 <sami.alam10@gmail.com>
2023-07-18 14:31:04 -0600

    First commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import '@/styles/globals.scss'
+import{ Provider } from "react-redux";
+import store from "../store";
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+let persistor = persistStore(store);
+function App({ Component, pageProps }) {
+  return (
+  <Provider store={store}>
+    <PersistGate Loading={null} persistor={persistor}>
+      <Component {...pageProps} />
+      </PersistGate>
+      </Provider>);
 }
+
+export default App;
